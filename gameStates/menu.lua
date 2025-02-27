f31225a4d17579741177cb8f9db869d503ed4015
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-02-27 18:42:19 -0300

    Loaded loveExpanded

---
+++ gameStates/menu.lua
@@ -0,0 +1,23 @@
+local thisState = {}
+
+local menuButtons = {}
+
+
+function thisState.load()
+
+end
+
+function thisState.update()
+
+end
+
+function thisState.draw()
+
+end 
+
+function thisState.mousepressed(x, y, button, istouch, presses)
+
+end
+
+
+return thisState
